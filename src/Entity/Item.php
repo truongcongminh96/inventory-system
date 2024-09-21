58e2413c65ea74442b68df4393624cbc8cf616d0
truongcongminh96 <truongcongminh96@gmail.com>
2024-09-21 14:29:29 +0700

    Create migration class Item

---
+++ src/Entity/Item.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ItemRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ItemRepository::class)]
+class Item
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $sellIn = null;
+
+    #[ORM\Column]
+    private ?int $quality = null;
+
+    private string $imgUrl;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSellIn(): ?int
+    {
+        return $this->sellIn;
+    }
+
+    public function setSellIn(int $sellIn): static
+    {
+        $this->sellIn = $sellIn;
+
+        return $this;
+    }
+
+    public function getQuality(): ?int
+    {
+        return $this->quality;
+    }
+
+    public function setQuality(int $quality): static
+    {
+        $this->quality = $quality;
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return (string) "{$this->name}, {$this->sellIn}, {$this->quality}";
+    }
+
+    public function getImgUrl(): string
+    {
+        return $this->imgUrl;
+    }
+
+    public function setImgUrl(string $imgUrl): self
+    {
+        $this->imgUrl = $imgUrl;
+
+        return $this;
+    }
+}
